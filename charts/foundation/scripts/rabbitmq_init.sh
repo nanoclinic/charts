dff758d7fa6ad0c0009a07ad3556112a772639e4
Vitor Pinto <vitor@nursa.com>
2024-05-03 16:23:35 -0300

    feat: allow cross-ns and named ips

--- charts/foundation/scripts/rabbitmq_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "overwriting variable"
-export RABBITMQ_PASSWOR=$(echo $RABBITMQ_PASSWORD | awk -F: '/password/ {print $4}'| sed -e "s/password//g" | sed -E 's/"|"}//g')
-rabbitmqctl change_password $RABBITMQ_USERNAME $RABBITMQ_PASSWOR
